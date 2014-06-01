c98c9ca275821d2375e4ca56805a17b84a1c4746
camilo <cgs.camiogonzalez@gmail.com>
2014-05-31 21:44:18 -0400

    added code to read the hall effect sensor and report back its value

--- FastBike.ino
+++ FastBike.ino
@@ -1,9 +1,12 @@
 char input[20];
 int index = 0;
+int hallEffectSensor = 2;
 
 void setup() {
   delay(10);
   Serial.begin(9600);
+  pinMode(hallEffectSensor, INPUT);
+  digitalWrite(hallEffectSensor, HIGH);
   Serial.println("Welcome to Smart Bike, by Camilo Gonzalez");
 }
 
@@ -18,9 +21,21 @@ void loop() {
       if(input[index] == -1) continue;
       index++;
     }
-    Serial.println("input ");
+    Serial.print("input ");
     Serial.println(input);
     memset(input, '\0', 10);
   }
+}
+
+boolean getHallEffect(int pin) {
+  boolean value = false;
+  int pinInput = digitalRead(pin);
+  
+  if(pinInput == 0) {
+    value = true;
+  }else {
+    value = false;
+  }
+  return value;
 }
 
